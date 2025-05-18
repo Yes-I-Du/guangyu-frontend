807be2adb8216828844463e32122c44f5efc9d36
Yes-I-Du <dumz999@163.com>
2025-05-18 13:36:58 +0800

    前台服务初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="App">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from './layouts/BasicLayout.vue'
+import { healthUsingGet } from '@/api/mainController'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+</script>
